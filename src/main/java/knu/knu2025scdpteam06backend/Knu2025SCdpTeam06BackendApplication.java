9924db57c808a76ef525765488decb803ad1447e
윤강훈 <hoon@Hoons-MacBook-Air.local>
2025-04-07 01:43:32 +0900

    Init: initial commit

---
+++ src/main/java/knu/knu2025scdpteam06backend/Knu2025SCdpTeam06BackendApplication.java
@@ -0,0 +1,13 @@
+package knu.knu2025scdpteam06backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Knu2025SCdpTeam06BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Knu2025SCdpTeam06BackendApplication.class, args);
+    }
+
+}
