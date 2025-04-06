fdd3a29f9303601bf08986312c71dd268c3ed05c
윤강훈 <hoon@Hoons-MacBook-Air.local>
2025-04-07 01:43:32 +0900

    Init: initial commit

---
+++ src/test/java/knu/knu2025scdpteam06backend/Knu2025SCdpTeam06BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package knu.knu2025scdpteam06backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Knu2025SCdpTeam06BackendApplicationTests {
+
+    @Test
+    void contextLoads( {
+    }
+
+}
