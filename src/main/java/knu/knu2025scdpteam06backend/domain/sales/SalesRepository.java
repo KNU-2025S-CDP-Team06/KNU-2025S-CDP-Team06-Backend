0fdaee225468ccc268ad7cb685a28c1167b9bbb8
YoonGangHoon <yghun021007@naver.com>
2025-04-09 03:00:08 +0900

    Feat: 1회 판매 데이터 CRUD

---
+++ src/main/java/knu/knu2025scdpteam06backend/domain/sales/SalesRepository.java
@@ -0,0 +1,13 @@
+package knu.knu2025scdpteam06backend.domain.sales;
+
+import knu.knu2025scdpteam06backend.domain.dailydata.DailyData;
+import knu.knu2025scdpteam06backend.domain.menu.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SalesRepository extends JpaRepository<Sales, Long> {
+    Optional<Sales> findByDailyDataAndMenu(DailyData dailyData, Menu menu;
+}
