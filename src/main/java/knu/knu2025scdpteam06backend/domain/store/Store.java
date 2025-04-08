0dd86ebd5c3e87de2f91e95fbd6db38ee2ed8cbd
YoonGangHoon <yghun021007@naver.com>
2025-04-09 02:59:04 +0900

    Feat: 매장 정보 CRUD

---
+++ src/main/java/knu/knu2025scdpteam06backend/domain/store/Store.java
@@ -0,0 +1,35 @@
+package knu.knu2025scdpteam06backend.domain.store;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "store"
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Store {
+
+    @Id
+    private Long id; // 매장 ID
+
+    private String mbId; // 사업자번호
+
+    private String name; // 매장명
+
+    private String address; // 매장 도로명 주소
+
+    private String latitude;
+
+    private String longitude;
+
+    @Column(nullable = true
+    private Integer cluster;
+}
