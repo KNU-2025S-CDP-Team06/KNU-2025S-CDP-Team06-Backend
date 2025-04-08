8a50bcb22088f474bff0d5759c46239e2dab5a6b
YoonGangHoon <yghun021007@naver.com>
2025-04-09 02:59:37 +0900

    Feat: 일일 매출 CRUD

---
+++ src/main/java/knu/knu2025scdpteam06backend/domain/dailydata/DailyData.java
@@ -0,0 +1,35 @@
+package knu.knu2025scdpteam06backend.domain.dailydata;
+
+import jakarta.persistence.*;
+import knu.knu2025scdpteam06backend.domain.store.Store;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "daily_data", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"storeId", "date"})
+})
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DailyData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    private LocalDateTime dateTime;
+
+    private Integer totalRevenue;
+
+    private Integer totalCount;
+}
