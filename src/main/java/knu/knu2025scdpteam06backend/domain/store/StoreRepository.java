6f85b7a7f70e1e4a267f06d2f12cb46897b6e17c
YoonGangHoon <yghun021007@naver.com>
2025-04-09 02:59:04 +0900

    Feat: 매장 정보 CRUD

---
+++ src/main/java/knu/knu2025scdpteam06backend/domain/store/StoreRepository.java
@@ -0,0 +1,10 @@
+package knu.knu2025scdpteam06backend.domain.store;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Long> {
+}
