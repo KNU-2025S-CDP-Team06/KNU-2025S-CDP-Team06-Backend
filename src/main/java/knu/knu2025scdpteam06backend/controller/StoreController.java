047f0b01bbbb6a126a9cf65c58ca794f0a79aa54
YoonGangHoon <yghun021007@naver.com>
2025-04-30 19:23:42 +0900

    Chore: swagger 설명 추가

--- src/main/java/knu/knu2025scdpteam06backend/controller/StoreController.java
+++ src/main/java/knu/knu2025scdpteam06backend/controller/StoreController.java
@@ -1,5 +1,6 @@
 package knu.knu2025scdpteam06backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import knu.knu2025scdpteam06backend.dto.StoreResponseDto;
 import knu.knu2025scdpteam06backend.service.StoreService;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +19,13 @@ public class StoreController {
         this.storeService = storeService;
     }
 
+    @Operation(
+            summary = "특정 ID의 매장 정보 조회",
+            description = "ID를 이용하여 매장의 상세 정보를 조회합니다."
+    
     @GetMapping("/{id}"
-    public ResponseEntity<StoreResponseDto> getStore(@PathVariable Long id {
+    public ResponseEntity<StoreResponseDto> getStore(
+            @PathVariable Long id {
         StoreResponseDto store = storeService.getStoreById(id;
         return ResponseEntity.ok(store;
     }
