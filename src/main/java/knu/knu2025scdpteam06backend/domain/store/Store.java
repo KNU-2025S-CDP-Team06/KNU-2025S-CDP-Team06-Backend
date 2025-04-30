2370bc33fef9feae1afd4268cca6fe4bc3a99474
YoonGangHoon <yghun021007@naver.com>
2025-05-01 01:19:26 +0900

    Chore: Patch를 위한 Setter 추가

--- src/main/java/knu/knu2025scdpteam06backend/domain/store/Store.java
+++ src/main/java/knu/knu2025scdpteam06backend/domain/store/Store.java
@@ -4,14 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name = "store")
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
