7692e3988029f83b63a7eb14867df227277584b8
YoonGangHoon <yghun021007@naver.com>
2025-05-01 01:59:49 +0900

    Chore: swagger에 도메인 추가

--- src/main/java/knu/knu2025scdpteam06backend/config/SwaggerConfig.java
+++ src/main/java/knu/knu2025scdpteam06backend/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package knu.knu2025scdpteam06backend.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,13 +15,17 @@ public class SwaggerConfig {
     public OpenAPI openAPI( {
         return new OpenAPI(
                 .components(new Components(
-                .info(apiInfo(;
+                .info(apiInfo(
+                .servers(List.of(
+                new Server(.url("https://moki-api.mojan.kr".description("Production HTTPS 서버",
+                new Server(.url("http://localhost:3006".description("로컬 개발 서버"
+        ;
     }
 
     private Info apiInfo( {
         return new Info(
                 .title("종합설계프로젝트1 6조"
-                .description("매출 예측 시스템 api."
+                .description("매출 예측 시스템 api"
                 .version("1.0.0";
     }
 }
