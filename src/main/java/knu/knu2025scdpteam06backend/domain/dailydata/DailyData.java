68ecf3ba34e19f1ee741013740db7293fe532fba
YoonGangHoon <yghun021007@naver.com>
2025-05-02 21:29:38 +0900

    Feat: 일일 판매 데이터 엔티티 추가

--- src/main/java/knu/knu2025scdpteam06backend/domain/dailydata/DailyData.java
+++ src/main/java/knu/knu2025scdpteam06backend/domain/dailydata/DailyData.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "daily_data", uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"storeId", "date"}
+        @UniqueConstraint(columnNames = {"store_id", "date_time"}
 }
 @Getter
 @NoArgsConstructor
