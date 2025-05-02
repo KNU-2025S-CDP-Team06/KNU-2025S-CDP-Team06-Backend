45fa024163c62f1c1e4e130f82c5e34f52903579
YoonGangHoon <yghun021007@naver.com>
2025-05-02 21:44:27 +0900

    Feat: 에측 엔티티 추가

---
+++ src/main/java/knu/knu2025scdpteam06backend/domain/prediction/Forecast.java
@@ -0,0 +1,35 @@
+package knu.knu2025scdpteam06backend.domain.prediction;
+
+import jakarta.persistence.*;
+import knu.knu2025scdpteam06backend.domain.store.Store;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "forecast"
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Forecast {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY
+    @JoinColumn(name = "store_id", nullable = false
+    private Store store;
+
+    private LocalDateTime date;
+
+    private Integer prophetForecast;
+
+    private Double xgboostForecast;
+
+
+}
---
+++ src/main/java/knu/knu2025scdpteam06backend/domain/prediction/ForecastRepository.java
@@ -0,0 +1,6 @@
+package knu.knu2025scdpteam06backend.domain.prediction;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ForecastRepository extends JpaRepository<Forecast, Long> {
+}
