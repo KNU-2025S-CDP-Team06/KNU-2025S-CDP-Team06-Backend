f47637b778fbaf1fdd8c7d357b805b75dac7fda3
YoonGangHoon <yghun021007@naver.com>
2025-05-03 18:05:31 +0900

    Feat: 날씨 엔티티 추가

---
+++ src/main/java/knu/knu2025scdpteam06backend/domain/weather/Weather.java
@@ -0,0 +1,29 @@
+package knu.knu2025scdpteam06backend.domain.weather;
+
+import jakarta.persistence.*;
+import knu.knu2025scdpteam06backend.domain.store.Store;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Weather {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY
+    @JoinColumn(name = "store_id", nullable = false
+    private Store store;
+
+    private LocalDateTime date;
+
+    private Integer weekday;
+
+    private Double feeling;
+
+    @Column(nullable = true
+    private Double precipitation;
+
+    private String weather;
+}
