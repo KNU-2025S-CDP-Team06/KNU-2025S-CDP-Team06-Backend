a9766c92b2bc0fbc8a3506d821a58000408f0581
YoonGangHoon <yghun021007@naver.com>
2025-05-07 19:16:01 +0900

    Feat: 내일 예측값 반환 기능 추가

---
+++ src/main/java/knu/knu2025scdpteam06backend/controller/ForecastController.java
@@ -0,0 +1,25 @@
+package knu.knu2025scdpteam06backend.controller;
+
+import knu.knu2025scdpteam06backend.dto.forecast.ForecastResponseDto;
+import knu.knu2025scdpteam06backend.service.ForecastService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ForecastController {
+
+    private final ForecastService forecastService;
+
+    @GetMapping("/forecast/{id}")
+    public ForecastResponseDto getForecast(
+            @PathVariable String id,
+            @RequestParam String date
+    ) {
+        return forecastService.getForecastByStore(id, date);
+    }
+
+}
