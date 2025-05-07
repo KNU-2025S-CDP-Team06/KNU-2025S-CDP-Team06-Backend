545f297029dc92d98d9c174b6071a837f74fdb72
YoonGangHoon <yghun021007@naver.com>
2025-05-07 16:30:24 +0900

    Feat: 기간 내 모든 판매 데이터 반환 기능 추가

--- src/main/java/knu/knu2025scdpteam06backend/controller/SalesController.java
+++ src/main/java/knu/knu2025scdpteam06backend/controller/SalesController.java
@@ -2,6 +2,7 @@ package knu.knu2025scdpteam06backend.controller;
 
 import knu.knu2025scdpteam06backend.dto.sales.SalesRequestDto;
 import knu.knu2025scdpteam06backend.dto.sales.SalesResponseDto;
+import knu.knu2025scdpteam06backend.dto.sales.TotalSalesResponseDto;
 import knu.knu2025scdpteam06backend.service.SalesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -32,4 +33,23 @@ public class SalesController {
 
         return salesService.getSalesByStore(id, requestDto;
     }
+
+    @GetMapping("/total/{id}"
+    public TotalSalesResponseDto getTotalSales(
+            @PathVariable String id,
+            @RequestParam String startDate,
+            @RequestParam String endDate,
+            @RequestParam(required = false Integer startHour,
+            @RequestParam(required = false Integer endHour
+    {
+        SalesRequestDto requestDto = SalesRequestDto.builder(
+                .startDate(startDate
+                .endDate(endDate
+                .startHour(startHour
+                .endHour(endHour
+                .build(;
+
+        return salesService.getTotalSalesByStore(id, requestDto;
+    }
+
 }
