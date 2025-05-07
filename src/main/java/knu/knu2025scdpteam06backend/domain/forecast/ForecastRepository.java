5ae87857b7808a38246932d96edac8c7a954f663
YoonGangHoon <yghun021007@naver.com>
2025-05-07 19:16:01 +0900

    Feat: 내일 예측값 반환 기능 추가

--- src/main/java/knu/knu2025scdpteam06backend/domain/forecast/ForecastRepository.java
+++ src/main/java/knu/knu2025scdpteam06backend/domain/forecast/ForecastRepository.java
@@ -1,6 +1,11 @@
 package knu.knu2025scdpteam06backend.domain.forecast;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
+@Repository
 public interface ForecastRepository extends JpaRepository<Forecast, Long> {
+    Forecast getForecastsByStoreIdAndDate(Long storeId, LocalDateTime date);
 }
