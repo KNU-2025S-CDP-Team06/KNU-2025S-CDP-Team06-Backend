3b46513cba09f4e02784cc179e6b6a6276689e11
YoonGangHoon <yghun021007@naver.com>
2025-05-07 23:32:24 +0900

    Refactor: 코드 단순화

--- src/main/java/knu/knu2025scdpteam06backend/dto/store/StoreResponseDto.java
+++ src/main/java/knu/knu2025scdpteam06backend/dto/store/StoreResponseDto.java
@@ -1,17 +1,13 @@
 package knu.knu2025scdpteam06backend.dto.store;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 public class StoreResponseDto {
     private Long id;
     private String mbId;
     private String name;
     private String address;
-
-    public StoreResponseDto(Long id, String mbId, String name, String address) {
-        this.id = id;
-        this.mbId = mbId;
-        this.name = name;
-        this.address = address;
-    }
 }
