3031921fb83ef2f7bf4b0939ec7004e2ce02dbfd
YoonGangHoon <yghun021007@naver.com>
2025-05-09 00:08:36 +0900

    Refactor: 날짜 데이터 형식을 LocalDateTime으로 통일

--- src/main/java/knu/knu2025scdpteam06backend/domain/sales/SalesRepository.java
+++ src/main/java/knu/knu2025scdpteam06backend/domain/sales/SalesRepository.java
@@ -1,15 +1,12 @@
 package knu.knu2025scdpteam06backend.domain.sales;
 
-import knu.knu2025scdpteam06backend.domain.dailydata.DailyData;
-import knu.knu2025scdpteam06backend.domain.menu.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface SalesRepository extends JpaRepository<Sales, Long> {
-    List<Sales> findByStoreIdAndDatetimeBetween(Long id, LocalDateTime start, LocalDateTime end);
+    List<Sales> findByStoreIdAndDateTimeBetween(Long storeId, LocalDateTime start, LocalDateTime end);
 }
--- src/main/java/knu/knu2025scdpteam06backend/dto/sales/SalesRequestDto.java
+++ src/main/java/knu/knu2025scdpteam06backend/dto/sales/SalesRequestDto.java
@@ -2,12 +2,13 @@ package knu.knu2025scdpteam06backend.dto.sales;
 
 import lombok.Builder;
 import lombok.Getter;
+import java.time.LocalDateTime;
 
 @Getter
 @Builder
 public class SalesRequestDto {
-    String startDate;
-    String endDate;
+    LocalDateTime startDate;
+    LocalDateTime endDate;
     Integer startHour;
     Integer endHour;
 }
