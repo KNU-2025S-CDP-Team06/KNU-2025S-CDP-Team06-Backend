4ac2acc526c1dcf030608488de23db74ccacdf29
YoonGangHoon <yghun021007@naver.com>
2025-05-14 00:37:43 +0900

    Fix: 로그를 출력하던 문제 수정

--- src/main/java/knu/knu2025scdpteam06backend/service/AuthService.java
+++ src/main/java/knu/knu2025scdpteam06backend/service/AuthService.java
@@ -20,8 +20,6 @@ public class AuthService {
                 authRequestDto.getMbId(), authRequestDto.getPassword())
                 .orElseThrow(() -> new IllegalArgumentException("아이디 또는 비밀번호가 일치하지 않습니다."));
 
-        System.out.println(authRequestDto.getMbId());
-
         return AuthResponseDto.builder()
                 .token(store.getMbId()) // 실제로는 JWT 토큰 등을 생성해야 함
                 .build();
