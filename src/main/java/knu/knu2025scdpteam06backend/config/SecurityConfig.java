846bd05ea616254576bb3f273d579954d43f2f0f
YoonGangHoon <yghun021007@naver.com>
2025-05-17 18:53:57 +0900

    Feat: 인증된 사용자만 접근하도록 설정 변경

--- src/main/java/knu/knu2025scdpteam06backend/config/SecurityConfig.java
+++ src/main/java/knu/knu2025scdpteam06backend/config/SecurityConfig.java
@@ -1,23 +1,36 @@
 package knu.knu2025scdpteam06backend.config;
 
+import knu.knu2025scdpteam06backend.security.handler.CustomAuthenticationEntryPoint;
+import knu.knu2025scdpteam06backend.security.jwt.JwtAuthenticationFilter;
+import knu.knu2025scdpteam06backend.security.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtTokenProvider jwtTokenProvider) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers("/auth/**", "/swagger-ui/**", "/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
                 );
+
         return http.build();
     }
 }
