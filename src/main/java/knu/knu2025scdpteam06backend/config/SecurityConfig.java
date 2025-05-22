0f96a982035f62ae9ce699364b704745ab9c5d77
YoonGangHoon <yghun021007@naver.com>
2025-05-23 03:55:57 +0900

    Feat: CORS 설정 추가

--- src/main/java/knu/knu2025scdpteam06backend/config/SecurityConfig.java
+++ src/main/java/knu/knu2025scdpteam06backend/config/SecurityConfig.java
@@ -15,6 +15,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -27,16 +32,34 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic(Customizer.withDefaults())
+                .cors(Customizer.withDefaults()) // CORS 설정 활성화
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/admin/login", "/login","/auth/**", "/swagger-ui/**", "/api-docs/**").permitAll()
+                        .requestMatchers("/admin/login", "/login", "/auth/**", "/swagger-ui/**", "/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:5173", "https://moki.mojan.kr", "https://moki-api.mojan.kr"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("Authorization", "x-auth-token")); // 클라이언트에서 읽을 수 있는 헤더
+        configuration.setAllowCredentials(true); // 쿠키나 인증 헤더 허용
+        configuration.setMaxAge(3600L); // Preflight 요청 캐시 시간 (초)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 요청 경로에 대해 적용
+
+        return source;
     }
 
     @Bean
